1e6dd4a3bb5bee6bdf3a57befc39b04bd511adb8
Chandrika <chandrikanagaraga14@gmail.com>
2025-03-19 13:53:21 +0530

    Add files via upload

---
+++ Second.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+class Second{
+    public static void Main(String[] args){
+        for(int i = 0; a<=5 ; i++){
+            System.out.println(a);
+        }
+    }
+}
